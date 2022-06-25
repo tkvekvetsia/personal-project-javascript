d5c824192104e12ade3b59c5c82bbec37309ca74
tkvekvetsia <kvekvetsiatoma@gmail.com>
2022-06-25 12:46:25 -0700

    add LMS.add method in LMS class

--- index.mjs
+++ index.mjs
@@ -1,8 +1,16 @@
-import { Subject } from "./lms.mjs";
+import { Subject, LMS } from "./lms.mjs";
+
 const history = new Subject({
     title: 'History',
     lessons: 24,
     // description : "string"
 })
-console.log(history);
-console.log(history.id)
+
+// console.log(history)
+// console.log(history.id)
+
+const lms = new LMS();
+lms.add(history);
+
+// console.log(LMS.subjects)
+
--- lms.mjs
+++ lms.mjs
@@ -1,9 +1,20 @@
-import { validateSubject } from "./validator.mjs";
+import { validateSubject, lmsSubjValidator } from "./validator.mjs";
 export class Subject{
-    #counter = 1;
+    static counter = 1;
     constructor(subj){
         validateSubject(subj);
-        this.id = String(this.#counter++);
+        this.id = String(Subject.counter++);
         Object.assign(this, subj)
+    }
+}
+
+export class LMS{
+    static counter = 1;
+    static subjects = new Map();
+    
+    add(subj){
+        lmsSubjValidator(subj);
+        let id = String(LMS.counter++);
+        LMS.subjects.set(id, subj)
     }
 }
